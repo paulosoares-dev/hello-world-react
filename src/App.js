5599e64b49e2c57545cebcf1b46c96ecc1ac061d
Paulo Soares <contato@ptek.com.br>
2023-09-26 15:40:41 -0300

    Pequenas alterações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <p>React JS</p>
       </header>
+
+      <main>
+        <section className='box-titulo'>
+          <h1>React JS</h1>
+          <p>Biblioteca JavaScript para criar SPAs</p>
+
+          <a href='https://reactjs.org/' target="_blank">Site Oficial</a>
+        </section>
+
+        <section className='box-requisitos'>
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para aprender React JS:</p>
+
+          <div className='box-cards'>
+            <div className='card'>
+              <h3>HTML</h3>
+              <p>Aprender a estruturar os elementos da página com HTML</p>
+            </div>
+            <div className='card'>
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+            <div className='card'>
+              <h3>JavaScript</h3>
+              <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p className="autor">Rodapé React JS</p>
+      </footer>
     </div>
   );
 }
